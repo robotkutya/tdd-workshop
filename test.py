be04b31834551991de1293ade3376a07dd4cb285
Thomas Bean <thomas.bean@prezi.com>
2018-03-23 16:03:16 +0100

    test update with update map with neighbs

--- test.py
+++ test.py
@@ -14,6 +14,19 @@ class TestGameOfLife(unittest.TestCase):
 
         self.assertItemsEqual(game.generate_cell_neighbors(cell_coordinates), neighbors)
 
+    def test_update_map_with_neighbours(self):
+        map = set()
+        cell_coord = (0,0)
+        map.add(cell_coord)
+        game = Game(map)
+
+        game.update_map()
+
+        neighbors = [(1,1), (1,0), (1,-1), (0,1), (0,-1), (-1,1), (-1,0), (-1,-1)]
+        new_map = neighbors.append(cell_coord)
+
+        self.assertItemsEqual(game.map, new_map)
+
 
 if __name__ == '__main__':
     unittest.main()
